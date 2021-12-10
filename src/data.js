8093a39e3e87ece2b95c2f4f1db7129eab364f8d
Elibabah <elibabah@localhost.localdomain>
2021-12-10 14:14:16 -0600

    efectos navegacion aside

--- src/data.js
+++ src/data.js
@@ -6,129 +6,129 @@ let temasArray = [];
 
 //----------- Función limpiar generacion ----------//
 export let cleanGeneration = () => {
-  document.getElementById("generations").innerHTML = "";
+    document.getElementById("generations").innerHTML = "";
 };
 
 //----------- Función limpiar estudiantes --------//
 let cleanStudents = () => {
-  document.getElementById("student").innerHTML = "";
+    document.getElementById("student").innerHTML = "";
 };
 
 //----------- Función limpiar progresoArray --------//
 let cleanProgreso = () => {
-  progresoArray = [];
+    progresoArray = [];
 };
 
 //----------- Función limpiar datos en sedes --------//
 let cleanDataSede = () => {
-  document.getElementById("box").innerHTML = "";
+    document.getElementById("box").innerHTML = "";
 };
 
 //------------------------------- F E T C H - A - D A T A - A P I -------------------------------//
 
 export const alumnasWild = () => {
-  fetch(ourData)
-    .then((response) => response.json())
-    .then((data) => {
-      traerSedes(data);
-      // wildCodeCamp(data, sede)
-      // Pasar data a array vacío
-      dataToArray.push(data);
-      console.log(dataToArray);
-    })
-    .catch((error) => console.log(error));
+    fetch(ourData)
+        .then((response) => response.json())
+        .then((data) => {
+            traerSedes(data);
+            // wildCodeCamp(data, sede)
+            // Pasar data a array vacío
+            dataToArray.push(data);
+            console.log(dataToArray);
+        })
+        .catch((error) => console.log(error));
 };
 //---------------------------- Función iterar sedes --------------------------//
 
 export const traerSedes = (nuestraData) => {
-  console.log(nuestraData);
+    console.log(nuestraData);
 
-  let img;
-  let map;
+    let img;
+    let map;
 
-  for (const key in nuestraData) {
-    console.log(key);
-    if (key == "ajusco") {
-      img = "../assets/Ajusede.png";
-      map =
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13028.629039471927!2d-99.22165552222965!3d19.22005729385488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdfc6a5214f00d%3A0xff7bc254e041e012!2sAjusco!5e0!3m2!1ses-419!2smx!4v1638376655643!5m2!1ses-419!2smx";
-    } else if (key == "chapultepec") {
-      img = "../assets/chapSede.png";
-      map =
-        "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d240902.65895694678!2d-99.194448!3d19.364647!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6859c9c6d53b1683!2sChapultepec!5e0!3m2!1ses-419!2smx!4v1638933500745!5m2!1ses-419!2smx";
-    } else if (key == "iztapalapa") {
-      img = "../assets/iztaSede.png";
-      map =
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.517374251381!2d-99.08200685035888!3d19.34673803686901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fdf4b0edf02d%3A0xd78ca8ed33730ff4!2sIztapalapa%2C%2009858%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1638377614641!5m2!1ses-419!2smx";
-    }
+    for (const key in nuestraData) {
+        console.log(key);
+        if (key == "ajusco") {
+            img = "../assets/Ajusede.png";
+            map =
+                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13028.629039471927!2d-99.22165552222965!3d19.22005729385488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdfc6a5214f00d%3A0xff7bc254e041e012!2sAjusco!5e0!3m2!1ses-419!2smx!4v1638376655643!5m2!1ses-419!2smx";
+        } else if (key == "chapultepec") {
+            img = "../assets/chapSede.png";
+            map =
+                "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d240902.65895694678!2d-99.194448!3d19.364647!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6859c9c6d53b1683!2sChapultepec!5e0!3m2!1ses-419!2smx!4v1638933500745!5m2!1ses-419!2smx";
+        } else if (key == "iztapalapa") {
+            img = "../assets/iztaSede.png";
+            map =
+                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.517374251381!2d-99.08200685035888!3d19.34673803686901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fdf4b0edf02d%3A0xd78ca8ed33730ff4!2sIztapalapa%2C%2009858%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1638377614641!5m2!1ses-419!2smx";
+        }
 
-    document.getElementById("sedes").innerHTML += `
+        document.getElementById("sedes").innerHTML += `
             <sede>
                 <h1>Sede ${key}</h1>
                 <img id="sedAx" src="${img}" class="img-fluid" alt="imgAjusco" onclick="dashboard.generacion('${key}')" /><br />
                 <div id="map"><iframe style="border-radius:2%" src="${map}" width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 </div>
                 </sede>`;
-  }
+    }
 };
 
 //----------------------- Funcion para traer generaciones -----------------------//
 export const traerGeneraciones = (sede) => {
-  console.log(sede);
-  //Hidden screen
-  document.getElementById("screenDash").hidden = false;
-  document.getElementById("sedes-screen").hidden = true;
+    console.log(sede);
+    //Hidden screen
+    document.getElementById("screenDash").hidden = false;
+    document.getElementById("sedes-screen").hidden = true;
 
-  // Pasar data a array vacío de generaciones
-  generacionesArray.push(dataToArray[0][sede].generacion);
-  console.log(generacionesArray);
+    // Pasar data a array vacío de generaciones
+    generacionesArray.push(dataToArray[0][sede].generacion);
+    console.log(generacionesArray);
 
-  //Iterar generación
-  for (let generacion in dataToArray[0][sede].generacion) {
-    console.log(generacion);
-    console.log(dataToArray[0][sede].generacion);
-    //Limpiar data por sede
-    cleanDataSede();
-    cleanStudents();
+    //Iterar generación
+    for (let generacion in dataToArray[0][sede].generacion) {
+        console.log(generacion);
+        console.log(dataToArray[0][sede].generacion);
+        //Limpiar data por sede
+        cleanDataSede();
+        cleanStudents();
 
-    //Ícono por sede
-    let iconSede;
+        //Ícono por sede
+        let iconSede;
 
-    let totalAlum;
-    let porcentajeCompletado;
-    let alum90;
-    let alum60;
+        let totalAlum;
+        let porcentajeCompletado;
+        let alum90;
+        let alum60;
 
-    if (sede == "ajusco") {
-      iconSede = "../assets/axus.png";
-      totalAlum = "44";
-      porcentajeCompletado = "71.07%";
-      alum90 = "Yasmin Opaline, Iseult Ysolt";
-      alum60 =
-        "Heidi Sarah, Laurissa Natalee, Lora Christabel, May Pamella, Sydnie Jerri, Haidee Táhirih, Yolanda Zula";
-    } else if (sede == "chapultepec") {
-      iconSede = "../assets/chap.png";
-      totalAlum = "45";
-      porcentajeCompletado = "69.64%";
-      alum90 = "Peta Sylvia, Joy Cassarah";
-      alum60 =
-        "Rachael Cate, Phillida Melina,Salina Larissa, Vicki Annice, Clementina Anselma, Darla Merche, Michele Avice, Rebeccanne Cearra";
-    } else if (sede == "iztapalapa") {
-      iconSede = "../assets/iztap.png";
+        if (sede == "ajusco") {
+            iconSede = "../assets/axus.png";
+            totalAlum = "44";
+            porcentajeCompletado = "71.07%";
+            alum90 = "Yasmin Opaline, Iseult Ysolt";
+            alum60 =
+                "Heidi Sarah, Laurissa Natalee, Lora Christabel, May Pamella, Sydnie Jerri, Haidee Táhirih, Yolanda Zula";
+        } else if (sede == "chapultepec") {
+            iconSede = "../assets/chap.png";
+            totalAlum = "45";
+            porcentajeCompletado = "69.64%";
+            alum90 = "Peta Sylvia, Joy Cassarah";
+            alum60 =
+                "Rachael Cate, Phillida Melina,Salina Larissa, Vicki Annice, Clementina Anselma, Darla Merche, Michele Avice, Rebeccanne Cearra";
+        } else if (sede == "iztapalapa") {
+            iconSede = "../assets/iztap.png";
 
-      totalAlum = "45";
-      porcentajeCompletado = "68.97%";
-      alum90 = "Rachael Cate";
-      alum60 =
-        "Cari Candyce, Vicki Annice, Aileen Edwyna, Clementina Anselma, Darla Merche, Fran Laraine, Celinda Emperatriz, Joy Cassarah, Kyla Cori";
-    }
+            totalAlum = "45";
+            porcentajeCompletado = "68.97%";
+            alum90 = "Rachael Cate";
+            alum60 =
+                "Cari Candyce, Vicki Annice, Aileen Edwyna, Clementina Anselma, Darla Merche, Fran Laraine, Celinda Emperatriz, Joy Cassarah, Kyla Cori";
+        }
 
-    document.getElementById(
-      "generations"
-    ).innerHTML += `<a onclick="dashboard.alumnas('${generacion}')"class="d-block text-dark p-3"
+        document.getElementById(
+            "generations"
+        ).innerHTML += `<a onclick="dashboard.alumnas('${generacion}')"class="d-block text-dark p-3" id="asideGen"
 ><i class="fas fa-user-graduate mr-2 lead"></i>${generacion} generación</a>`;
-    document.getElementById("nombreSede").innerHTML = "Sede" + " " + sede;
-    document.getElementById("icono-sede").innerHTML = `<img
+        document.getElementById("nombreSede").innerHTML = "Sede" + " " + sede;
+        document.getElementById("icono-sede").innerHTML = `<img
             src="${iconSede}"
             alt=""
             width="70"
@@ -136,9 +136,9 @@ export const traerGeneraciones = (sede) => {
             class="d-inline-block align-text-top"
         />`;
 
-    document.getElementById(
-      "box"
-    ).innerHTML += `<div class="col-lg-3 col-md-6 d-flex stat my-3">
+        document.getElementById(
+            "box"
+        ).innerHTML += `<div class="col-lg-3 col-md-6 d-flex stat my-3">
                                         <div class="mx-auto">
 
                                             <h6 class="text-muted">Estudiantes registrados en esta sede</h6>
@@ -169,32 +169,32 @@ export const traerGeneraciones = (sede) => {
                                         </div>
                                     </div>
     `;
-  }
+    }
 };
 //----------------------- Funcion para traer alumnas por generación -----------------------//
 export const traerAlumnas = (generacion) => {
-  console.log(generacion);
-  //Iterar alumnas del array
+    console.log(generacion);
+    //Iterar alumnas del array
 
-  // Pasar data a array vacío de progreso
-  progresoArray.push(generacionesArray[0][generacion].estudiantes);
-  console.log(progresoArray);
-  console.log(generacionesArray[0][generacion].estudiantes);
+    // Pasar data a array vacío de progreso
+    progresoArray.push(generacionesArray[0][generacion].estudiantes);
+    console.log(progresoArray);
+    console.log(generacionesArray[0][generacion].estudiantes);
 
-  //Encendemos funcion sort
-  ordenar(progresoArray[0]);
+    //Encendemos funcion sort
+    ordenar(progresoArray[0]);
 
-  //Limpiamos progresoArray
-  cleanProgreso();
-  cleanStudents();
+    //Limpiamos progresoArray
+    cleanProgreso();
+    cleanStudents();
 
-  //Iteramos generacionesArray para acceder a alumnas
-  for (let [index, alumnas] of generacionesArray[0][
-    generacion
-  ].estudiantes.entries()) {
-    console.log(index, alumnas);
-    //progresoArray = "";
-    document.getElementById("student").innerHTML += `
+    //Iteramos generacionesArray para acceder a alumnas
+    for (let [index, alumnas] of generacionesArray[0][
+            generacion
+        ].estudiantes.entries()) {
+        console.log(index, alumnas);
+        //progresoArray = "";
+        document.getElementById("student").innerHTML += `
 
         <div class="card" style="width: 13rem;">
             <img src="../assets/2995914.png" class="card-img-top" alt="alumna_foto">    
@@ -227,27 +227,27 @@ export const traerAlumnas = (generacion) => {
 </div>
 
 `;
-  }
+    }
 };
 
 //--------------Función sort para ordenar cards alfabeticamente---------------//
 
 const ordenar = (studentsArray) => {
-  console.log(studentsArray);
+    console.log(studentsArray);
 
-  studentsArray.sort((a, b) => {
-    //Limpiar cards sort
+    studentsArray.sort((a, b) => {
+        //Limpiar cards sort
 
-    let nameA = a.nombre.toUpperCase();
-    let nameB = b.nombre.toUpperCase();
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-    return 0;
-  });
+        let nameA = a.nombre.toUpperCase();
+        let nameB = b.nombre.toUpperCase();
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    });
 };
 
 //-------------------Función para buscar estudiantes---------------------//
@@ -304,11 +304,11 @@ busqueda.addEventListener("keyup", searchStudents);*/
 };
 */
 export const progress = (generacion) => {
-  console.log(generacion);
+    console.log(generacion);
 
-  for (let key in progresoArray[0]) {
-    console.log(key, progresoArray[0]);
-  }
+    for (let key in progresoArray[0]) {
+        console.log(key, progresoArray[0]);
+    }
 };
 
 /*export const progress = (studentsArray) => {
